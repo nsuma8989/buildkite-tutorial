e792a0eebf86a7eaab6fa7caaf886bf6994d8bc9
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2024-10-10 12:16:17 -0700

    Update watch-github-release.sh

--- src/watch-github-release.sh
+++ src/watch-github-release.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-set -eux
+# Define log file name and target size (1MB = 1048576 bytes)
+LOGFILE="large_log_file.log"
+TARGET_SIZE=$((1024 * 1024))  # 1 MB in bytes
 
-: "${GITHUB_REPO:?required}"
-GITHUB_REPO="${GITHUB_REPO#https://}"
-metadata_key_github_repo_version="${GITHUB_REPO}::latest-version"
+# Create or clear the log file
+> "$LOGFILE"
 
-latest_release_json="$(curl -sSL "https://$GITHUB_REPO/releases/latest" -H 'Accept: application/json')"
-latest_release_tag=$(echo "$latest_release_json" | jq -r ".tag_name")
-latest_release_version="${latest_release_tag#v}"
-latest_release_version="${latest_release_version#go}"
-echo "$latest_release_version"
+# Loop until the file size is greater than 1MB
+while [ $(stat -c%s "$LOGFILE") -le $TARGET_SIZE ]
+do
+  # Append random data to the log file
+  echo "$(date) - Random log entry: $RANDOM" >> "$LOGFILE"
+done
 
-[[ $(buildkite-agent meta-data exists "$metadata_key_github_repo_version") ]] && {
-  previous_release_version="$(buildkite-agent meta-data get "$metadata_key_github_repo_version")"
-  echo "$previous_release_version"
-}
-
-exit 0
+# Output the size of the generated log file
+echo "Generated log file size: $(stat -c%s "$LOGFILE") bytes"
