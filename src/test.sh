2249f82ee9f3e91b8ae9fee3b0ae330c8d958566
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2025-04-22 11:37:28 -0700

    Create test.sh

---
+++ src/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+buildkite-agent pipeline upload .buildkite/trigger1.yaml
+buildkite-agent pipeline upload .buildkite/trigger2.yaml
+buildkite-agent pipeline upload .buildkite/trigger3.yaml
+buildkite-agent pipeline upload .buildkite/trigger4.yaml
